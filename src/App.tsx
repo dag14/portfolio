5a8c7ca353d2b16070b88464a1e188848cd9d0a6
Dagim Asrat <dagimasrat3@gmail.com>
2025-04-15 09:18:32 +0300

    future proof the app with tanstack/react for future dynamic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+
+const queryClient = new QueryClient();
+
 const App = () => (
-<div>
-  This is my portfolio
-</div>
+  <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+  </QueryClientProvider>
 );
 
 export default App;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,8 @@
+
+const Index = () => {
+  return (
+      <div>THIS IS THE INDEX PAGE</div>
+  );
+};
+
+export default Index;
